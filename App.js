e4eabb9058df5b28c4a2ab5b5538dddbc58e17a0
hewmx <mxhew20@gmail.com>
2023-11-11 12:13:13 +1300

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WebView from "react-native-webview";
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View
+          style={styles.container}
+      >
+        <WebView
+            style={styles.webContainer}
+            source={{ uri: 'https://capstone-react-frontend.vercel.app/' }}
+          />
+        </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Constants.statusBarHeight,
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
+  webContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
