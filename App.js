0c46e8ecab6ce2ad4f3223117c678c0bc722e72c
hewmx <mxhew20@gmail.com>
2023-11-12 13:08:55 +1300

    Implemented notification system

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import WebView from "react-native-webview";
 import Constants from 'expo-constants';
 
+import * as Notification from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+import {useEffect, useState} from "react";
+import * as Notifications from "expo-notifications";
+import registerNNPushToken, {registerIndieID} from 'native-notify';
+import * as SecureStore from 'expo-secure-store';
+import axios from "axios";
+import {APP_ID, APP_TOKEN, BASE_URL} from "./constants";
+
 export default function App() {
+    const [expoToken, setExpoToken] = useState()
+
+
+    useEffect(() => {
+        getUserToken()
+    }, []);
+
+    async function getUserToken() {
+        let token = await SecureStore.getItemAsync('user_token')
+        if (!token) {
+            await axios.get(BASE_URL + 'create_alert_profile/', {
+
+            }).then(async function (response) {
+                let token = response.data.expoUserToken
+                let subId = response.data.subscriptionToken
+                setExpoToken(token)
+                await SecureStore.setItemAsync('user_token', token)
+                await registerIndieID(subId, APP_ID, APP_TOKEN)
+                alert("Token created! " + token + " / " + subId)
+            }).catch(error => alert(error))
+
+        }
+        else {
+            alert(token)
+            await axios.post(BASE_URL + 'get_alert_profile/', {
+                expo_token: token,
+            }).then(async function (response) {
+                let token = response.data.expoUserToken
+                let subId = response.data.subscriptionToken
+                setExpoToken(token)
+                await registerIndieID(subId, APP_ID, APP_TOKEN)
+                alert("Token exists! " + token + " / " + subId)
+            }).catch(error => console.log(error))
+        }
+    }
+
   return (
       <View
           style={styles.container}
       >
-        <WebView
+          <Text>Hello</Text>
+          <WebView
             style={styles.webContainer}
             source={{ uri: 'https://capstone-react-frontend.vercel.app/' }}
           />
-        </View>
+      </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -30,3 +78,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   }
 });
+
+
---
+++ constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://mxhew.pythonanywhere.com/api/"
+export const APP_ID = 14719
+export const APP_TOKEN = 'fS59VdsG6wSUsAUbYddt18'
